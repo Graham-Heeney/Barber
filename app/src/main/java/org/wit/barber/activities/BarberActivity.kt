264ab181380215df700e1fcf39df27ce61a92e1c
Graham-Heeney <20102466@mail.wit.ie>
2025-10-21 15:56:40 +0100

    added cancle button and added barberAdapter to own package

--- app/src/main/java/org/wit/barber/activities/BarberActivity.kt
+++ app/src/main/java/org/wit/barber/activities/BarberActivity.kt
@@ -1,8 +1,10 @@
 package org.wit.barber.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
 import org.wit.barber.databinding.ActivityBarberBinding
 import org.wit.barber.main.MainApp
 import org.wit.barber.models.BarberModel
@@ -20,10 +22,8 @@ class BarberActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         app = application as MainApp
-
-        i("Barber Activity started...")
-
-
+        binding.toolbar.title = "Add Barber"
+        setSupportActionBar(binding.toolbar)
 
         binding.btnAdd.setOnClickListener {
             barber.title = binding.barberTitle.text.toString()
@@ -31,15 +31,26 @@ class BarberActivity : AppCompatActivity() {
             if (barber.title.isNotEmpty()) {
                 app.barbers.add(barber.copy())
                 i("Add Button Pressed: $barber")
-                for (i in app.barbers.indices) {
-                    i("Barber[$i]: ${this.app.barbers[i]}")
-                }
                 setResult(RESULT_OK)
                 finish()
             } else {
                 Snackbar.make(it, "Please Enter a title", Snackbar.LENGTH_LONG).show()
             }
         }
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(org.wit.barber.R.menu.menu_barber, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            org.wit.barber.R.id.item_cancel -> {
+                setResult(RESULT_CANCELED)
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
