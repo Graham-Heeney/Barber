0c250474d2370321005fd6baeb9d2142ee5dd66f
Graham-Heeney <20102466@mail.wit.ie>
2025-12-16 19:46:34 +0000

    Recover location not wokring

--- app/src/main/java/org/wit/barber/activities/BarberActivity.kt
+++ app/src/main/java/org/wit/barber/activities/BarberActivity.kt
@@ -26,6 +26,7 @@ class BarberActivity : AppCompatActivity() {
     private lateinit var imageIntentLauncher : ActivityResultLauncher<Intent>
     private lateinit var mapIntentLauncher : ActivityResultLauncher<Intent>
 
+    var location = Location(52.245696, -7.139102, 15f)
 
     var barber = BarberModel()
     lateinit var app: MainApp
@@ -39,11 +40,11 @@ class BarberActivity : AppCompatActivity() {
             i ("Set Location Pressed")
         }
         binding.barberLocation.setOnClickListener {
-            val location = Location(52.245696, -7.139102, 15f)
             val launcherIntent = Intent(this, MapActivity::class.java)
                 .putExtra("location", location)
             mapIntentLauncher.launch(launcherIntent)
         }
+
 
 
 
@@ -93,7 +94,19 @@ class BarberActivity : AppCompatActivity() {
     private fun registerMapCallback() {
         mapIntentLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult())
-            { i("Map Loaded") }
+            { result ->
+                when (result.resultCode) {
+                    RESULT_OK -> {
+                        if (result.data != null) {
+                            i("Got Location ${result.data.toString()}")
+                            //location = result.data!!.extras?.getParcelable("location",Location::class.java)!!
+                            location = result.data!!.extras?.getParcelable("location")!!
+                            i("Location == $location")
+                        } // end of if
+                    }
+                    RESULT_CANCELED -> { } else -> { }
+                }
+            }
     }
 
 
