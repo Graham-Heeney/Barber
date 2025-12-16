94e8ee724a50eaa4461fa3c78ae9737e77927a79
Graham-Heeney <20102466@mail.wit.ie>
2025-12-16 19:49:59 +0000

    Map recovers location now

--- app/src/main/java/org/wit/barber/activities/MapActivity.kt
+++ app/src/main/java/org/wit/barber/activities/MapActivity.kt
@@ -1,5 +1,7 @@
 package org.wit.barber.activities
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,13 +10,15 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import org.wit.barber.R
 import org.wit.barber.databinding.ActivityMapBinding
 import org.wit.barber.models.Location
+import androidx.activity.addCallback
 
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback,  GoogleMap.OnMarkerDragListener {
 
     private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapBinding
@@ -29,6 +33,14 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        onBackPressedDispatcher.addCallback(this ) {
+            val resultIntent = Intent()
+            resultIntent.putExtra("location", location)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        }
+
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -40,6 +52,17 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             .draggable(true)
             .position(loc)
         map.addMarker(options)
+        map.setOnMarkerDragListener(this)
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, location.zoom))
     }
+
+    override fun onMarkerDrag(p0: Marker) {  }
+
+    override fun onMarkerDragEnd(marker: Marker) {
+        location.lat = marker.position.latitude
+        location.lng = marker.position.longitude
+        location.zoom = map.cameraPosition.zoom
+    }
+
+    override fun onMarkerDragStart(p0: Marker) {  }
 }
