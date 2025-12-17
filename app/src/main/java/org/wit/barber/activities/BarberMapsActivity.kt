26f287090948176f258a81d34dc1e6c2ac1d9d55
Graham-Heeney <20102466@mail.wit.ie>
2025-12-17 15:04:24 +0000

    The map is now displaying all barbers and opens on the latest one

--- app/src/main/java/org/wit/barber/activities/BarberMapsActivity.kt
+++ app/src/main/java/org/wit/barber/activities/BarberMapsActivity.kt
@@ -2,8 +2,12 @@ package org.wit.barber.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import org.wit.barber.R
+import org.wit.barber.main.MainApp
 
 import org.wit.barber.databinding.ActivityBarberMapsBinding
 import org.wit.barber.databinding.ContentBarberMapsBinding
@@ -11,6 +15,8 @@ import org.wit.barber.databinding.ContentBarberMapsBinding
 class BarberMapsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBarberMapsBinding
+    private lateinit var map: GoogleMap
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,9 +27,26 @@ class BarberMapsActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.title = getString(R.string.title_activity_barber_maps)
 
-        // ✅ LAB WAY: access included layout via binding
         binding.contentBarberMaps.mapView.onCreate(savedInstanceState)
+        app = application as MainApp
+
+        binding.contentBarberMaps.mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
     }
+
+    private fun configureMap() {
+        map.uiSettings.isZoomControlsEnabled = true
+        app.barbers.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.title).position(loc)
+            map.addMarker(options)?.tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+
+        }
+    }
+
 
     override fun onResume() {
         super.onResume()
@@ -50,6 +73,3 @@ class BarberMapsActivity : AppCompatActivity() {
         binding.contentBarberMaps.mapView.onSaveInstanceState(outState)
     }
 }
-
-
-
